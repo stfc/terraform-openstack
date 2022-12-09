cb5f04b4ca0c4c987f502a045f3935b8e668ca42
Elizabeth Mamtsits <elizabeth.mamtsits@stfc.ac.uk>
2022-12-09 10:54:17 +0000

    Terraform deployment for new instance with volume
    Boots Instance and Attach Existing Volume as a Block Device

--- providers.tf
+++ providers.tf
@@ -13,15 +13,29 @@ terraform {
 provider "openstack" {
   }
 
-  resource "openstack_compute_instance_v2" "exec" {
-
-    name            = "${var.VM_name}-${count.index}"
-    image_name        = var.image_name
-    flavor_name       = var.flavor_name
-    key_pair        = var.key_pair
-    security_groups = ["default"]
-    count           = var.number_of_VMs
+  resource "openstack_compute_instance_v2" "instance" {
+    name              = "${var.VM_name}"
+    image_id          = "${var.image_id}"
+    flavor_name       = "${var.flavor_name}"
+    key_pair          = "${var.key_pair}"
+    security_groups   = ["default"]
   
+    block_device {
+      uuid                  = "${var.image_id}"
+      source_type           = "image"
+      destination_type      = "local"
+      boot_index            = 0
+      delete_on_termination = true
+    }
+  
+    block_device {
+      source_type           = "blank"
+      destination_type      = "volume"
+      volume_size           = 1
+      boot_index            = 1
+      delete_on_termination = true
+    }
+
     network {
       name = "Internal"
     }
--- variable.tf
+++ variable.tf
@@ -8,15 +8,23 @@ variable "VM_name" {
 default = "openstack-VM"
 }
 
-variable "number_of_VMs" {
-default = "2"
+variable "vol_name" {
+default = "test-tf-vol"	
 }
 
-variable "image_name" {
-default = "ubuntu-focal-20.04-nogui"
+variable "vol_size" {
+default = "1"		
 }
-// scientificlinux-7-nogui
-// ubuntu-focal-20.04-gui
+
+variable "number_of_VMs" {
+default = "1"
+}
+
+variable "image_id" {
+default = "0eedaf19-2ead-44bf-84e4-3334ad772da1" //ubuntu-focal-20.04-nogui
+}
+// "0e50b86d-f923-4cfa-a96b-17086258521e" scientificlinux-7-nogui
+// "49669b19-2ac9-4578-ae11-01af778fa447" ubuntu-focal-20.04-gui
 
 variable "flavor_name" {
 default = "l3.nano"
